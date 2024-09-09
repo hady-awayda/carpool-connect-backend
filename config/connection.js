1c133d2316693f377dbb4acc5ac838ca3ee49622
hady-awayda <hady_awayda@hotmail.com>
2024-09-09 12:54:02 +0300

    Updated pipeline

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,14 @@
 import mariadb from "mariadb";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: "../.env" });
 
 const pool = mariadb.createPool({
-  host:
-    process.env.DB_HOST ||
-    "carpool-dev.cruwuwwcyxr8.eu-central-1.rds.amazonaws.com",
-  user: process.env.DB_USER || "admin",
-  password: process.env.DB_PASSWORD || "sefactory",
-  database: process.env.DB_NAME || "carpool_dev",
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   connectionLimit: 5,
 });
 

5202e5e543c00546e2aa43a82bf400ad467f9a6f
hady-awayda <hady_awayda@hotmail.com>
2024-09-18 02:40:00 +0300

    Implemented authentication and authorization middlewares

--- src/middleware/adminAuthorization.js
+++ src/middleware/adminAuthorization.js
@@ -1,14 +1,13 @@
-// import authentication from "./authentication.js";
+import authentication from "./authentication.js";
 
-// // Middleware for admin authorization
-// const adminAuthorization = (req, res, next) => {
-//   authentication(req, res, () => {
-//     if (req.user && req.user.role === "admin") {
-//       next();
-//     } else {
-//       res.status(403).json({ message: "Admin authorization required" });
-//     }
-//   });
-// };
+const adminAuthorization = (req, res, next) => {
+  authentication(req, res, () => {
+    if (req.user && req.user.role === "admin") {
+      next();
+    } else {
+      res.status(403).json({ message: "Admin authorization required" });
+    }
+  });
+};
 
-// export default adminAuthorization;
+export default adminAuthorization;
--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,25 +1,25 @@
-// import jwt from "jsonwebtoken";
-// import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
-// const authentication = (req, res, next) => {
-//   const authHeader = req.header("Authorization");
+const authentication = (req, res, next) => {
+  const authHeader = req.header("Authorization");
 
-//   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//     return res.status(401).json({ message: "No token, authentication denied" });
-//   }
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token, authentication denied" });
+  }
 
-//   const token = authHeader.split(" ")[1];
+  const token = authHeader.split(" ")[1];
 
-//   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-//     if (err) {
-//       return res.status(403).json({ message: "Invalid token" });
-//     }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
 
-//     req.user = decoded.user;
-//     next();
-//   });
-// };
+    req.user = decoded.user;
+    next();
+  });
+};
 
-// export default authentication;
+export default authentication;

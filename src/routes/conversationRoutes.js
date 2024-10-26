7a6a352480e644c4089d5e0f40e0204f995ceb61
hady-awayda <hady_awayda@hotmail.com>
2024-10-26 20:29:42 +0300

    Updated conversations router with appropriate routes for the api

--- src/routes/conversationRoutes.js
+++ src/routes/conversationRoutes.js
@@ -4,50 +4,46 @@ import userAuthorization from "../middleware/userAuthorization.js";
 
 const router = express.Router();
 
-router.post(
-  "/createConversation",
-  userAuthorization,
-  ConversationController.createConversation
-);
+// router.post(
+//   "/create",
+//   userAuthorization,
+//   ConversationController.createConversation
+// );
+
+router.get("/", userAuthorization, ConversationController.getUserConversations);
 
 router.get(
-  "/findConversation/:userId2",
+  "/find/:userId2",
   userAuthorization,
   ConversationController.findConversationByUserIds
 );
 
 router.get(
-  "/userConversations",
-  userAuthorization,
-  ConversationController.getUserConversations
-);
-
-router.get(
-  "/getConversations/:conversationId",
-  userAuthorization,
-  ConversationController.getConversationById
-);
-
-router.post(
-  "/sendMessage/:conversationId",
-  userAuthorization,
-  ConversationController.sendMessage
-);
-
-router.get(
-  "/getMessages/:conversationId",
+  "/messages/:conversationId",
   userAuthorization,
   ConversationController.getMessagesByConversationId
 );
 
+// router.get(
+//   "/getConversations/:conversationId",
+//   userAuthorization,
+//   ConversationController.getConversationById
+// );
+
+router.post(
+  "/message/:conversationId",
+  userAuthorization,
+  ConversationController.sendMessage
+);
+
 router.delete(
-  "/deleteMessage/:messageId",
+  "/message/:messageId",
   userAuthorization,
   ConversationController.softDeleteMessage
 );
 
 router.delete(
-  "/deleteConversation/:conversationId",
+  "/conversation/:conversationId",
   userAuthorization,
   ConversationController.softDeleteConversation
 );
